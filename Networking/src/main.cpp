eac1953f7fd3ccafcd4119d59ed379a90cb12bb5
Mateus Alves <mateus.alves.unb@gmail.com>
2024-06-15 11:26:46 -0300

    Adjusting project name. Also, including SFML lib and building a simple program to test

--- Networking/src/main.cpp
+++ Networking/src/main.cpp
@@ -4,8 +4,32 @@
 #include <asio/ts/buffer.hpp>
 #include <asio/ts/internet.hpp>
 
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Network.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Audio.hpp>
+
 int main()
 {
     std::cout << "Hello world\n";
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
     return 0;
 }
